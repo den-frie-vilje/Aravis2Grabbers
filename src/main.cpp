6974424add0b5d6aa11b22f14ae3fa053a72ebbe
ole kristensen <ole@kristensen.name>
2021-09-29 09:44:44 +0200

    First commit

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,13 @@
 
 //========================================================================
 int main( ){
-
-	ofSetupOpenGL(1920,1024, OF_WINDOW);			// <-------- setup the GL context
+	ofGLWindowSettings settings;
+	settings.setGLVersion(3,1);
+	ofCreateWindow(settings);
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
 	// pass in width and height too:
-	ofRunApp( new ofApp());
+	ofRunApp(new ofApp());
 
 }
